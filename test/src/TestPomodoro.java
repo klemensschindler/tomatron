dd64a59272eb2e14aeb973f2fa46dad404c73704
MaikoID <maiko.cezar@gmail.com>
2015-08-31 23:42:49 -0300

    MVC wit TEST

--- test/src/TestPomodoro.java
+++ test/src/TestPomodoro.java
@@ -4,7 +4,7 @@ import tomatron.model.*;
 
 public class TestPomodoro {
 	public static void main(String[] args) {
-		IChronometerCreator icc = PomodoroFactory();
+		IChronometerCreator icc = new PomodoroFactory();
 		IPomodoro p = (IPomodoro) icc.createPomodoro();
 		p.start();
 		p.pause();
